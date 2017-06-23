c05c2c622131a81fb51e2656d488d22e3623e712
hippoom <hippoom@gmail.com>
2017-06-23 10:31:35 +0800

    spike on multi event handler

--- src/main/java/org/restbucks/wechat/bff/wechat/messaging/NewSubscribedEventHandler.java
+++ src/main/java/org/restbucks/wechat/bff/wechat/messaging/NewSubscribedEventHandler.java
@@ -1,12 +1,8 @@
 package org.restbucks.wechat.bff.wechat.messaging;
 
-import static java.lang.String.format;
-
 import java.util.Map;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.SneakyThrows;
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpKefuService;
@@ -22,43 +18,28 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "app")
 @RequiredArgsConstructor
-public class QrCodeScannedEventHandler implements WxMpMessageHandler {
+public class NewSubscribedEventHandler implements WxMpMessageHandler {
 
     private final WxMpKefuService kefuService;
 
     @Setter
     private String publicBaseUri = "https://wechat.restbucks.org";
 
-    @SneakyThrows
-    public void handle(String eventKey, String fromUser) {
-        String storeNumber = getStore(eventKey);
-        kefuService.sendKefuMessage(
-            WxMpKefuMessage.NEWS().addArticle(assembleArticle(storeNumber)).toUser(fromUser)
-                .build());
-    }
-
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context,
         WxMpService wxMpService, WxSessionManager sessionManager) throws WxErrorException {
-
-        handle(wxMessage.getEventKey(), wxMessage.getFromUser());
-
+        kefuService.sendKefuMessage(
+            WxMpKefuMessage.NEWS().addArticle(assembleArticle())
+                .toUser(wxMessage.getFromUser())
+                .build());
         return null;
     }
 
-    private WxArticle assembleArticle(String storeNumber) {
+    private WxArticle assembleArticle() {
         WxArticle article = new WxArticle();
-        article.setTitle(format("Welcome to Restbucks Store %s", storeNumber));
-        article.setUrl(publicBaseUri + "/index.html#/place-order-form/" + storeNumber);
+        article.setTitle("Welcome to Restbucks");
+        article.setUrl(publicBaseUri + "/index.html");
         article.setPicUrl("https://avatars3.githubusercontent.com/u/18325423?v=3&s=200");
         return article;
-    }
-
-    private String getStore(String eventKey) {
-        return Optional.ofNullable(eventKey) // "qrscene_store_123"
-            .map(k -> k.substring(k.indexOf("_") + 1)) // getting "store_123"
-            .map(k -> k.substring(k.indexOf("_") + 1)) // getting "123"
-            .orElseThrow(() -> new IllegalArgumentException(
-                "Cannot extract store number due to empty qr code key"));
     }
 }
--- src/main/java/org/restbucks/wechat/bff/wechat/messaging/WeChatMessagingConfiguration.java
+++ src/main/java/org/restbucks/wechat/bff/wechat/messaging/WeChatMessagingConfiguration.java
@@ -1,5 +1,6 @@
 package org.restbucks.wechat.bff.wechat.messaging;
 
+import lombok.RequiredArgsConstructor;
 import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.api.WxMpService;
@@ -7,21 +8,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class WeChatMessagingConfiguration {
+
+    private final QrCodeScannedEventHandler qrCodeScannedEventHandler;
+    private final NewSubscribedEventHandler newSubscribedEventHandler;
 
     @Bean
     public WxMpMessageRouter wxMpMessageRouter(
-        QrCodeScannedEventHandler qrCodeScannedEventHandler,
         WxMpService wxMpService) {
-        // @formatter:off
         WxMpMessageRouter router = new WxMpMessageRouter(wxMpService);
+        configure(router);
+
+        return router;
+    }
+
+    private void configure(WxMpMessageRouter router) {
+        // @formatter:off
         router
             .rule()
                 .msgType(WxConsts.XML_MSG_EVENT)
-                .event(WxConsts.EVT_SCAN)
+                .event(WxConsts.EVT_SCAN).event(WxConsts.EVT_SUBSCRIBE)
                 .handler(qrCodeScannedEventHandler)
             .end();
         // @formatter:on
-        return router;
     }
 }
